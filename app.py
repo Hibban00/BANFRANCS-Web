3a76276f99818fe98ea046d38aee0ff4434aaff7
Hibban00 <N00487283@upn.pe>
2026-06-03 00:24:55 -0500

    Implementa colecciones y contador de carrito

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+
+@app.route("/colecciones")
+def colecciones():
+    return render_template("colecciones.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
